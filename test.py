6119d4b083d95e70141022f6655c35765867d9b5
Rob131313 <robert.adam@some.ox.ac.uk>
2018-06-07 20:00:17 +0100

    again

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
 print('test.py')
 # to upload the changes we've made
+# first you need to git add
+# then you need to git commit (which pushes to the local repository)
+# when doing this you don't need to follow with file names - it pushes those
+# files stored temporarily into the local repository
+# then git push pushes these into the cloud
+# to pull an updated version from the cloud, use git pull (no need for file
+# name)
+
+# but to really manage your code you need use branching
+# usually have 2 versions of code: production (which is used) and development
+# (which is changed and tested)
+# don't want to break entire software just to update little bits
+# git checkout -b <branch-name> - Create a new branch and switch to that
+# branch.
+#git checkout <branch-name> - Switch to the branch you specified.
+#git merge <merge-to-branch-name> - Merge current branch to the specified
+#branch.
--- test3.py
+++ test3.py
@@ -1,1 +1,7 @@
 print('this is test3')
+
+var_1 = input('Give me your name: ')
+var_2 = input('Give me your name: ')
+
+print('Your name is {0}'.format(var_1))
+print('Your age is {0}'.format(var_2))
